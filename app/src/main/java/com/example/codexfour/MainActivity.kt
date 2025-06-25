399ea4b1722e38862853a9968ffba1a9bd36a1da
DayVilla <payoneerofficialhassan@gmail.com>
2025-06-25 13:09:25 +0500

    message from github

--- app/src/main/java/com/example/codexfour/MainActivity.kt
+++ app/src/main/java/com/example/codexfour/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     //message from mac
     //message form new-branch
+    //message from github
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
