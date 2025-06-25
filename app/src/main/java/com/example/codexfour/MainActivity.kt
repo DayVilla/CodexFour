ffc1529616a4cfc03542570656bbf6b3d172a302
Hsss <payoneerofficialhassan@gmail.com>
2025-06-25 13:11:04 +0500

    second msg from new-branch

--- app/src/main/java/com/example/codexfour/MainActivity.kt
+++ app/src/main/java/com/example/codexfour/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     //message from mac
     //message form new-branch
+    //second message from new-branch
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
